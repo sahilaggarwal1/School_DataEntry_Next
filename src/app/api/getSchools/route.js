5845b7feb68152b5659dff2d5d04b25d1a32a9c0
Sahil Aggarwal <85857292+sahilaggarwal1@users.noreply.github.com>
2025-09-01 19:27:54 +0530

    Update route.js

--- src/app/api/getSchools/route.js
+++ src/app/api/getSchools/route.js
@@ -2,10 +2,13 @@ import { db } from "@/app/_lib/db";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    try {
-        const [rows] = await db.query("SELECT id, name, address, city, image FROM schools");
-        return NextResponse.json(rows);
-    } catch (err) {
-        return NextResponse.json({ error: err.message }, { status: 500 });
-    }
+  try {
+    const [rows] = await db.query(
+      "SELECT id, name, address, city, image FROM schools"
+    );
+    return NextResponse.json(rows);
+  } catch (err) {
+    console.error("DB ERROR:", err);  // 👈 log full error
+    return NextResponse.json({ error: err.message }, { status: 500 });
+  }
 }
