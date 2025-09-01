2ae4c11901e63a20b86eac0dfcc68762cb63670d
Sahil Aggarwal <85857292+sahilaggarwal1@users.noreply.github.com>
2025-09-01 19:05:38 +0530

    Update db.js

--- src/app/_lib/db.js
+++ src/app/_lib/db.js
@@ -7,7 +7,7 @@ export async function connectDB() {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    port: 10479,
     ssl: {
       ca: fs.readFileSync("ca.pem")  // upload this file in your project root
     }
